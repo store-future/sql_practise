f7c57c255c3ae0cb5957b54af202540c37eee939
dhananjay <126044351+store-future@users.noreply.github.com>
2023-12-26 23:32:06 +0530

    making sql python_sql -connection

---
+++ database_connection.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+# making connection with database
+cunn = mysql.connector.connect(host = 'localhost' , user = 'root' , password = 'dddddddd')
+
+
+def connection_conf():
+    # printing conection id 
+    connection_id = cunn.connection_id
+
+    #printing current version of mysql-server
+    server_version = cunn.get_server_version()
+
+    # checking do we connected or not
+    check_cunn = cunn.is_connected()
+
+    result = f"conection id :{connection_id} \ncurrent version of mysql-server :{server_version} \nchecking if connected or not : {check_cunn}"
+    return(result)
+# print(connection_conf())
+
+
+
